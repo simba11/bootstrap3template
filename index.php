df1d755d599c80d9a536359266269859edfdcae6
Sim Brar <sim@simbrar.com>
2014-01-17 12:31:54 -0800

    bootstrap template

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Bootstrap 101 Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="_/css/bootstrap.css" rel="stylesheet">
+    <link href="_/css/mystyles.css" rel="stylesheet">
+
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    
+    <script src="_/js/bootstrap.js"></script>
+    <script src="_/js/myscript.js"></script>
+
+  </body>
+</html>
